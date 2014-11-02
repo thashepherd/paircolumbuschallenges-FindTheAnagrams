e246df7f8db44fd6b61bea1b44ff4ee016fabcc9
Jennifer Cable <jcable@covermymeds.com>
2014-11-02 16:19:07 -0500

    made changes

--- anagram_detector.rb
+++ anagram_detector.rb
@@ -3,22 +3,24 @@
 
 def detect_anagram(word1, word2)
 
-  #canonical(word1) == canonical(word2)
+  canonical(word1) == canonical(word2)
   #Insert your solution below here.
+  #word1.downcase!
+  #word1.chars.sort == word2.chars.sort
 
 end
-
 
 # VERSION TWO
 # Implement this version by uncommenting the line in the function.
 # Then move your solution into the canonical function
 # Modify it in such a way that when called from detect_anagram it results in true or false
 
-
 def canonical(word)
- 
+
   #insert your solution here
- 
+
+  word.downcase.chars.sort
+
 end
 
 p "Do NOT change any code below this line"
@@ -27,5 +29,3 @@ p detect_anagram('glean', 'angel') == true
 p detect_anagram('pants', 'pants') == true
 p detect_anagram('CinEmA', 'iceman') == true
 p detect_anagram('defgh8', 'g8hefd') == true
-
-
