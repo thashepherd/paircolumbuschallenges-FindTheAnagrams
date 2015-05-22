e5bfe024575dcc4a8fc9ed36e85d3901ad330206
Peter Mueller <pmueller@covermymeds.com>
2015-05-22 14:15:05 -0400

    add anagram logic

--- anagram_detector.rb
+++ anagram_detector.rb
@@ -3,8 +3,7 @@
 
 def detect_anagram(word1, word2)
 
-  #canonical(word1) == canonical(word2)
-  #Insert your solution below here.
+  canonical(word1) == canonical(word2)
 
 end
 
@@ -16,9 +15,9 @@ end
 
 
 def canonical(word)
- 
-  #insert your solution here
- 
+
+  word.downcase.split(//).sort
+
 end
 
 p "Do NOT change any code below this line"
