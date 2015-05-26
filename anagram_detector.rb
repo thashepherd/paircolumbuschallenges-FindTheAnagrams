0d8d366af8b179cdb4c6900241841e3843a1dc71
Erin Smith <erinsmith8585@gmail.com>
2015-05-26 12:14:22 -0400

    Define canonical method with all true tests

--- anagram_detector.rb
+++ anagram_detector.rb
@@ -1,5 +1,6 @@
 # Implement this in such a way that when called below, detect_anagram will result in true or false.
 def canonical(word)
+	word.downcase.split('').sorts
 end
 
 def detect_anagram(word1, word2)
