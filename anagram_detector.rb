66500330c8a54b4dce33a685cbe369b96e45f241
Lovinder Pnag <lpnag@CMDVMOSXLT075.local>
2015-05-26 09:02:02 -0400

    finished challenge

--- anagram_detector.rb
+++ anagram_detector.rb
@@ -2,12 +2,8 @@
 # Implement your solution below
 
 def detect_anagram(word1, word2)
-
-  #canonical(word1) == canonical(word2)
-  #Insert your solution below here.
-
+	canonical(word1) == canonical(word2)
 end
-
 
 # VERSION TWO
 # Implement this version by uncommenting the line in the function.
@@ -16,16 +12,16 @@ end
 
 
 def canonical(word)
- 
+	word.downcase.split("").sort
   #insert your solution here
  
 end
 
 p "Do NOT change any code below this line"
 p detect_anagram('angel', 'glean') == true
+p detect_anagram('angel', 'glean')
 p detect_anagram('glean', 'angel') == true
 p detect_anagram('pants', 'pants') == true
 p detect_anagram('CinEmA', 'iceman') == true
 p detect_anagram('defgh8', 'g8hefd') == true
-
 
