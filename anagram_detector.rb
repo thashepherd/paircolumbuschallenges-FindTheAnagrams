b21c929b23a99d53dcd9e08f4598f32d9e2d9ae6
Bryon Wilkins <bwilkins@CMDVMOSXLT082.local>
2015-06-04 09:42:53 -0400

    Completed part 1 and 2

--- anagram_detector.rb
+++ anagram_detector.rb
@@ -1,9 +1,37 @@
-# Implement this in such a way that when called below, detect_anagram will result in true or false.
-def canonical(word)
-end
+# By Kaleb Davis and Bryon Wilkins
+# VERSION ONE
+# Implement your solution below
 
 def detect_anagram(word1, word2)
+
   canonical(word1) == canonical(word2)
+=begin  
+  #Insert your solution below here.
+    tally1 = Hash.new(0)
+  tally2 = Hash.new(0)
+  word1.downcase!
+  word2.downcase!
+  word1.split("").each do |letter|
+    tally1[letter]+=1
+  end
+  word2.split("").each do |letter|
+    tally2[letter]+=1
+  end
+  tally1.eql? tally2
+=end
+end
+
+
+# VERSION TWO
+# Implement this version by uncommenting the line in the function.
+# Then move your solution into the canonical function
+# Modify it in such a way that when called from detect_anagram it results in true or false
+
+
+def canonical(word)
+ 
+  word.downcase.chars.sort()
+ 
 end
 
 p "Do NOT change any code below this line"
@@ -12,5 +40,3 @@ p detect_anagram('glean', 'angel') == true
 p detect_anagram('pants', 'pants') == true
 p detect_anagram('CinEmA', 'iceman') == true
 p detect_anagram('defgh8', 'g8hefd') == true
-
-
