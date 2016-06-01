aae9ffd6a2d0bef106fbbb286a72aa75722e4d53
bwilkins <bwilkins@covermymeds.com>
2016-06-01 10:17:41 -0400

    refactored

--- anagram_detector.exs
+++ anagram_detector.exs
@@ -1,16 +1,16 @@
-defmodule Anagram do
+defmodule AnagramDetector do
   def canonical(word) do
-    Enum.sort String.split(String.downcase(word), ""), &(&1 < &2)
+    String.downcase(word) |> String.split("") |> Enum.sort(&(&1 < &2))
   end
 
-  def detect_anagram(word1, word2)do
-    Anagram.canonical(word1) == Anagram.canonical(word2)
+  def detect_anagram(word1, word2) do
+    canonical(word1) == canonical(word2)
   end
 end
 
-IO.puts Anagram.detect_anagram("angel", "glean") == true
-IO.puts Anagram.detect_anagram("glean", "angel") == true
-IO.puts Anagram.detect_anagram("pants", "pants") == true
-IO.puts Anagram.detect_anagram("abc", "def") == false
-IO.puts Anagram.detect_anagram("CinEma", "iceman") == true
-IO.puts Anagram.detect_anagram("888grbm", "gr8b8m8") == true
+IO.puts AnagramDetector.detect_anagram("angel", "glean") == true
+IO.puts AnagramDetector.detect_anagram("glean", "angel") == true
+IO.puts AnagramDetector.detect_anagram("pants", "pants") == true
+IO.puts AnagramDetector.detect_anagram("abc", "def") == false
+IO.puts AnagramDetector.detect_anagram("CinEma", "iceman") == true
+IO.puts AnagramDetector.detect_anagram("888grbm", "gr8b8m8") == true
